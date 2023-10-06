e31eae99b5198180c2d238499db38445fbdf951b
DavideNicoletta <davidenicoletta@gmail.com>
2023-10-06 17:34:18 +0200

    Sviluppo parziale della parte funzionale dell'lcs in python

--- Python/lcs.py
+++ Python/lcs.py
@@ -1,11 +1,53 @@
 
 
+def printTable(table) -> None:
+    for row in table:
+        for element in row:
+            print(element, end = ' ')
+        print()
+
 def buildTable(firstVector, secondVector) -> list:
     m = int(len(firstVector))
     n = int(len(secondVector))
-    
-    
+    table = [[0 for y in range(m + 1)] for x in range(n + 1)]
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if (i == 0 and j == 0) or (i == 0 and j != 0) or (i != 0 and j == 0):
+                table[i][j] = 0
+            elif firstVector[i - 1] == secondVector[j - 1]:
+                table[i][j] = table[i-1][j-1] + 1
+            elif table[i][j - 1] > table[i - 1][j]:
+                 table[i][j] = table[i][j - 1]
+            else:
+                table[i][j] = table[i - 1][j]
 
+    printTable(table)
+    return table
+
+def rebuiltTable(table, m, n, firstVector, secondVector, LCS) -> str:
+    if m == 0 and n == 0:
+        return ""
+    else:
+        if table[m][n] == table[m - 1][n] and table[m][n] == table[m][n - 1]:
+            if firstVector[m - 1] == secondVector[n - 1]: 
+                    LCS = firstVector[m - 1] + rebuiltTable(table, m, n, firstVector, secondVector, LCS)
+            else:
+                    LCS = rebuiltTable(table, m - 1, n, firstVector, secondVector, LCS)
+        else:
+            if firstVector[m - 1] == secondVector[n - 1]:
+                        LCS = firstVector[m - 1] + rebuiltTable(table, m, n, firstVector, secondVector, LCS)
+            else:
+                if table[m - 1][n] >= table[m][n - 1]:
+                    LCS = rebuiltTable(table, m - 1, n, firstVector, secondVector, LCS)
+                else:
+                    LCS = rebuiltTable(table, m, n - 1, firstVector, secondVector, LCS)
+    return LCS     
+                           
+def reverse(LCS) -> str:
+     LCSList = list(LCS)
+     string = ""
+     for x in LCSList:
+          string = string + x
 
 
 
@@ -14,5 +56,7 @@ string2 = input("Inserisci stringa 2\n")
 
 firstVector = list(string1)
 secondVector = list(string2)
-buildTable(firstVector, secondVector)
+print(reverse(
+     rebuiltTable(
+          buildTable(firstVector, secondVector), len(firstVector), len(secondVector), firstVector, secondVector, "")))
 
